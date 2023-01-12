3ab683bc1caec7d4335164206b4700cbd042650f
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2023-01-11 21:30:28 -0500

    Polishing the PDFStats page
    * show the title of the form, not the file name
    * slightly wider column, to fit more info on desktop (still works on mobile)
    * show the complexity score and it's individual components
    * give suggestions from OpenAI for the form
    * can link directly to an accordian section, and will open automatically

--- docassemble/PDFStats/pdfstats.py
+++ docassemble/PDFStats/pdfstats.py
@@ -1,13 +1,14 @@
 # pre-load
 
 from docassemble.webapp.app_object import app, csrf
-from docassemble.base.util import path_and_mimetype
+from docassemble.base.util import path_and_mimetype, get_config
 import json
 
 import os
 from flask import flash, request, redirect, url_for, render_template_string, Markup
 from werkzeug.utils import secure_filename
 import formfyxer
+from formfyxer import lit_explorer
 import textstat
 
 import pandas
@@ -102,15 +103,17 @@ def upload_file():
 
 from flask import send_from_directory
 
-@app.route('/pdfstats/view/<name>')
-def view_stats(name):
-    if not (name and allowed_file(name)):
+@app.route('/pdfstats/view/<filename>')
+def view_stats(filename):
+    if not (filename and allowed_file(filename)):
         raise Exception("Not a valid filename")
     path_to_file = os.path.join(
         app.config["PDFSTAT_UPLOAD_FOLDER"],
-        secure_filename(name),
+        secure_filename(filename),
     )
-    stats = formfyxer.parse_form(path_to_file, normalize=True, use_spot=False, debug=True)
+    stats = formfyxer.parse_form(path_to_file, normalize=True, debug=True, openai_creds=get_config("open ai"), spot_token=get_config("spot token"))
+    title = stats.get('title', filename)
+    complexity_score = formfyxer.form_complexity(stats)
     word_count = len(stats.get("text").split(" "))
     difficult_word_count = textstat.difficult_words(stats.get("text"))
     return f"""
@@ -119,7 +122,7 @@ def view_stats(name):
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Statistics for { name }</title>
+    <title>Stats for { title }</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <style>
     .suffolk-blue {{
@@ -127,7 +130,7 @@ def view_stats(name):
     }}
     </style>
   </head>
-<body class="text-center">
+<body>
 <nav class="navbar navbar-dark suffolk-blue">
   <div class="container-fluid">
     <a class="navbar-brand" href="https://suffolklitlab.org">
@@ -135,34 +138,37 @@ def view_stats(name):
   Suffolk LIT Lab
   </a>
   </div>
-</nav>    
+</nav>
 
-<main style="max-width: 500px; margin: auto;">
-<h1 class="pb-2 border-bottom">File statistics for <span class="text-break">{ name }</span></h1>
-<table class="table">
+<main style="max-width: 800px; margin-left: auto; margin-right: auto;">
+<h1 class="pb-2 border-bottom text-center">File statistics for <span class="text-break">{ title }</span></h1>
+<table class="table text-center">
     <thead>
     <tr>
       <th scope="col">Statistic name</th>
       <th scope="col">Value</th>
-      <th scope="col">Benchmark</th>
+      <th scope="col">Note</th>
     </tr>
     </thead>
     <tbody>
+    <tr>
+    <th scope="row">Complexity Score</th>
+    <td>{ "{:.2f}".format(complexity_score) }</td>
+    <td>Lower is better</td>
+    </tr>
     <tr>
     <th scope="row">Time to read</th>
     <td>
     About { minutes_to_hours(int(word_count / 150)) }
     </td>
-    <td>
-    </td>
+    <td>Assuming 150 words per minute reading speed.</td>
     </tr>
     <tr>
     <th scope="row">Time to answer</th>
     <td>
-    About { minutes_to_hours(round(stats.get("time to answer", (0,0))[0])) }, plus or minus { minutes_to_hours(round(stats.get("time to answer", (0,0))[1])) } (1 standard deviation)
+    About { minutes_to_hours(round(stats.get("time to answer", (0,0))[0])) }, plus or minus { minutes_to_hours(round(stats.get("time to answer", (0,0))[1])) }
     </td>
-    <td>
-    </td>
+    <td>The variation covers 1 standard deviation. See <a href="#flush-collapseThree">the footnotes</a> for more information.</td>
     </tr>
     <tr>
     <th scope="row">
@@ -204,14 +210,14 @@ def view_stats(name):
     Word count / page
     </th>
     <td>{ word_count } ({float(word_count/stats.get("pages",1.0)):.1f})</td>
-    <td>Users <a href="https://www.nngroup.com/articles/how-little-do-users-read/">read as little as 20% of the content</a> on a longer page. Try to keep word count to 110 words to have your reader absorb at least 50%.</td>
+    <td>Users <a href="https://www.nngroup.com/articles/how-little-do-users-read/">read as little as 20% of the content</a> on a longer page. Try to keep word count to 110 words.</td>
     </tr>
     <tr>
     <th scope="row">
-    Number of "difficult words" (includes inflections of some "easy" words)
+    Number of "difficult words"
     </th>
     <td>{ difficult_word_count } <br/> ({difficult_word_count/word_count * 100:.1f}%)</td>
-    <td>Target is < 5%</td>
+    <td>May include inflections of some "easy" words. Target is < 5%</td>
     </tr>
     <tr>
     <th scope="row">
@@ -229,7 +235,21 @@ def view_stats(name):
     </tr>
     </tbody>
 </table>
-<div class="accordion" id="fullTextAccordion">
+
+<h2 class="pb-2 border-bottom text-center">Ideas for Improvements</h2>
+
+{ "<p>Here's an idea for a new title: <b>" + stats["suggested title"] + "</b></p>" if stats.get("suggested title") else ""}
+
+<p>Here's a idea for an easy-to-read description of the form:
+<div class="card text-left">
+  <div class="card-body">
+   { stats["description"] }
+  </div>
+</div>
+</p>
+
+
+<div class="accordion text-center" id="fullTextAccordion">
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingOne">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
@@ -264,19 +284,52 @@ def view_stats(name):
       <div class="accordion-body">
       <p><b>Note: "time to answer" is drawn as a random sample from an assumed normal distribution of times to answer, with a pre-set standard deviation and mean that depends
       on the answer type and allowed number of characters. It will likely be a different number if you refresh and recalculate.</b></p>
-      { pandas.DataFrame.from_records(stats.get("debug fields")).to_html() }
+      { pandas.DataFrame.from_records(stats.get("debug fields")).to_html() if stats.get("debug fields") else [] }
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingFour">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+        Information about complexity
+      </button>
+    </h2>
+    <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingFour" data-bs-parent="#fullTextAccordion">
+      <div class="accordion-body">
+      <p><b>Note: this is a list of each metric and it's specific score; they are summed together to get the total score</b></p>
+      <table class="table">
+      <thead>
+        <tr>
+          <th>Metric name</th>
+          <th>Original value</th>
+          <th>Weighted value</th>
+        </tr>
+      </thead>
+      <tbody>
+        { chr(10).join(['<tr><th scope="row">{}</th><td>{:.2f}</td><td>{:.2f}</td></tr>'.format(v[0], v[1], v[2]) for v in lit_explorer._form_complexity_per_metric(stats)]) }
+      </tbody>
+      </table>
       </div>
     </div>
   </div>
 
 </div>
+<br/>
 
 
 <a class="btn btn-primary" href="/pdfstats" role="button">Upload a new PDF</a>
 
 </main>
-
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+    <script>
+        $(document).ready(function() {{
+            var accordSec = window.location.hash;
+            if (accordSec.length) {{
+                $(accordSec).collapse("show");
+            }}
+        }});
+    </script>
 </body>
 
 </html>
